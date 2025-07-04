0de95b6550e8c29bdda200b949fc04a3048e4a73
ABdul Waheed <ab.waheed1722@gmail.com>
2025-07-04 06:11:12 +0300

    style fix

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -21,7 +21,7 @@ export function HeroSection() {
             {/* Content Container: Applies max-w-7xl to the text/buttons */}
             <div className="relative z-10 h-full flex items-start justify-center">
                 <div className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 text-center h-full flex items-center justify-start">
-                    <Card className='rounded-lg bg-white/[.08] w-auto my-auto text-start py-6 px-10 border border-white/15 shadow-none'>
+                    <Card className='rounded-lg w-auto my-auto text-start py-6 px-10 stroke bg-transparent backdrop-blur-xs stroke-[#5B5B5B] shadow-none'>
                         <h1 className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-[64px] font-heading font-bold leading-24 uppercase">
                             Empowering Businesses
                             <br />
